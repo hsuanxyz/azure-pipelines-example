470f72bbfa52765f4bfbdd2d270cdd9a8099232d
Hsuan Lee <hsuangm@gmail.com>
2020-07-06 10:59:29 +0800

    chore: test preview

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'azure-pipelines-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('azure-pipelines-example');
+    expect(app.title).toEqual('Azure Pipelines Example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('azure-pipelines-example app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Azure Pipelines Example app is running!');
   });
 });
